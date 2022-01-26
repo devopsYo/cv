c125be34e1d8b95378af7eab17e2876168abeeda
Yoel <yoel_levy_1@hotmail.fr>
2022-01-26 22:30:38 +0200

    project cv without propTypes and font-awesome

---
+++ src/components/Section.js
@@ -0,0 +1,30 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import SectionItem from "./SectionItem"
+import Skill from "./Skill"
+
+const Section = ({ section }) => {
+    return (
+        <div className="part">
+            <div className="partTitle icon">
+                <div>
+                    
+                </div>
+                <h3>{section.sectionTitle}</h3>
+            </div>
+            {section.isSectionItem ?
+                section.sectionItems.map((sectionItem, index) =>
+                    <SectionItem key={index} sectionItem={sectionItem} />)
+                : <div className="subPart">
+                    {!section.isSkill ?
+                        <div>{section.sectionText}</div>
+                        : section.skills.map((skill, index) =>
+                            <Skill key={index} skill={skill} />)
+                    }
+                </div>
+
+            }
+        </div>
+    )
+}
+
+export default Section
